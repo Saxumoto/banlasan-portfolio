f2e1435b6957969658c662ff665c088b830b7a6f
Saxumoto <chelseabanlasan@gmail.com>
2025-12-15 23:42:53 +0800

    Fix build script for Next.js 16

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // This prevents the build from crashing by skipping strict image optimization
     unoptimized: true,
   },
 };

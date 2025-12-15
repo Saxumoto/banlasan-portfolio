4cb65cc0926096b776c7f7f8f76c3ddd59472c65
Saxumoto <chelseabanlasan@gmail.com>
2025-12-15 22:13:58 +0800

    fix memory crash

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    // This prevents the build from crashing by skipping strict image optimization
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
